0bf1bf669faeb9f3c5d203088d5cd9d8c68f5c15
kareemshawkii <168727752+kareemshawkii@users.noreply.github.com>
2025-05-16 16:21:17 +0300

    Update

--- Selenuim/src/test/java/Sort.java
+++ Selenuim/src/test/java/Sort.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
 import java.time.Duration;
 import java.util.*;
 import java.util.stream.Collectors;
